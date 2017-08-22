9498acd1e6d8d045623cc4b3c10eff5c4d1e8a0a
Stuart Powers <stuart.powers@gmail.com>
2017-08-22 16:42:10 -0400

    initial hack to support python3

--- build/interpreter.py
+++ build/interpreter.py
@@ -1,6 +1,14 @@
 import sys
 import json
-from itertools import izip, starmap, izip_longest, repeat
+import logging
+
+if sys.version_info.major == 2:
+    from itertools import izip, starmap, izip_longest, repeat
+else:
+    from itertools import starmap, zip_longest, repeat
+    izip = zip
+    izip_longest = zip_longest
+
 from datetime import datetime
 
 class FlowsheetsJSONEncoder(json.JSONEncoder):
@@ -40,54 +48,63 @@ user_globals = {
     '_iter_repeat': repeat,
 }
 
-with open(str(datetime.now())+'.txt', 'wb') as log_file:
+logger = logging.getLogger('FlowSheets')
 
-    while True:
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s')
+logging.root.setLevel(level=logging.INFO)
 
-        # I would use sys.stdin.readline() here, but it seems like there's a bug where
-        # if input is being sent in too fast then readlines takes incoming pieces instead of
-        # separating by new line.
-
-        accumulating_payload = [] 
-        next_character = sys.stdin.read(1)
-        # Data format is one line per command, with newlines within a command displayed as __NEWLINE__
-        while next_character != '\n':
-            accumulating_payload.append(next_character)
-            next_character = sys.stdin.read(1) # Note: I believe the program execution is paused while waiting for the next character.
-
-        payload = ''.join(accumulating_payload)
-        # payload = '__EXEC:a = 1'
-        # payload = '__EVAL:json.dumps(a)'
-
-        # payload = '__EXEC:def _b_function():__NEWLINE__x = 5__NEWLINE__return x'
-        # payload = '__EXEC:b = _b_function()'
-        # payload = '__EVAL:json.dumps(b)'
-
-        if payload:
-
-            cleaned_payload = payload.replace('__NEWLINE__', '\n')
-
-            try:
-                if cleaned_payload.startswith('__EXEC:'):
-                    cleaned_payload = cleaned_payload[7:]
-                    exec(cleaned_payload, user_globals)
-                    sys.stdout.write('Payload executed successfully: {}\n'.format(cleaned_payload.replace('\n', '__NEWLINE__')))
-                    sys.stdout.flush()
-
-                elif cleaned_payload.startswith('__EVAL:'):
-                    cleaned_payload = cleaned_payload[7:]
-                    sys.stdout.write(eval(cleaned_payload, user_globals)+'\n')
-                    sys.stdout.flush()
-
-                else:
-                    raise Exception('Payload did not start with execution instruction __EXEC or __EVAL')
+logger.info("running %s" % ' '.join(sys.argv))
 
 
-            except Exception as e:
-                sys.stderr.write('{}\n'.format(e))
-                sys.stderr.flush()
+#with open(str(datetime.now())+'.txt', 'wb') as log_file:
 
-            log_file.write(cleaned_payload+'\n')
-            log_globals = {key: value for key,value in user_globals.iteritems() if key not in ('__builtins__', 'stringify', 'izip', 'starmap', 'starfilter', '_iter_repeat')}
-            # log_file.write('current globals: {}\n'.format(log_globals))
-            log_file.flush()
+while True:
+
+    # I would use sys.stdin.readline() here, but it seems like there's a bug where
+    # if input is being sent in too fast then readlines takes incoming pieces instead of
+    # separating by new line.
+
+    accumulating_payload = [] 
+    next_character = sys.stdin.read(1)
+    # Data format is one line per command, with newlines within a command displayed as __NEWLINE__
+    while next_character != '\n':
+        accumulating_payload.append(next_character)
+        next_character = sys.stdin.read(1) # Note: I believe the program execution is paused while waiting for the next character.
+
+    payload = ''.join(accumulating_payload)
+    # payload = '__EXEC:a = 1'
+    # payload = '__EVAL:json.dumps(a)'
+
+    # payload = '__EXEC:def _b_function():__NEWLINE__x = 5__NEWLINE__return x'
+    # payload = '__EXEC:b = _b_function()'
+    # payload = '__EVAL:json.dumps(b)'
+
+    if payload:
+
+        cleaned_payload = payload.replace('__NEWLINE__', '\n')
+
+        try:
+            if cleaned_payload.startswith('__EXEC:'):
+                cleaned_payload = cleaned_payload[7:]
+                exec(cleaned_payload, user_globals)
+                sys.stdout.write('Payload executed successfully: {}\n'.format(cleaned_payload.replace('\n', '__NEWLINE__')))
+                sys.stdout.flush()
+
+            elif cleaned_payload.startswith('__EVAL:'):
+                cleaned_payload = cleaned_payload[7:]
+                sys.stdout.write(eval(cleaned_payload, user_globals)+'\n')
+                sys.stdout.flush()
+
+            else:
+                raise Exception('Payload did not start with execution instruction __EXEC or __EVAL')
+
+
+        except Exception as e:
+            sys.stderr.write('{}\n'.format(e))
+            sys.stderr.flush()
+
+        logger.info(cleaned_payload)
+        #log_file.write(cleaned_payload+'\n')
+        log_globals = {key: value for key,value in user_globals.items() if key not in ('__builtins__', 'stringify', 'izip', 'starmap', 'starfilter', '_iter_repeat')}
+        # log_file.write('current globals: {}\n'.format(log_globals))
+        #log_file.flush()
